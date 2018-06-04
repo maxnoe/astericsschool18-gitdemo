30a2c5b2bed94a7469dde357d2873b3c362775cf
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-06-04 11:41:55 +0200

    First working fib numbers

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,11 @@
-def fib():
-    return 42
+def fib(N):
+    if N == 0:
+        return 1
+    if N == 1:
+        return 1
+    return fib(N - 1) + fib(N - 2)
 
 
 if __name__ == "__main__":
-    fib()
+    for i in range(5):
+        print(fib(i))
